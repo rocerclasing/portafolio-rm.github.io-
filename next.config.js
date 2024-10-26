526f13d725a54fac7bf5f054bb5024a8b273bd7e
Robert <robertclasing@gmail.com>
2024-10-26 13:05:03 -0300

    Descripción de los cambios realizados

---
+++ next.config.js
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+
+const nextConfig = {
+  output: 'export',
+  reactStrictMode: true,
+  experimental: {
+    reactRefresh: false,
+  },
+  // Otras configuraciones pueden ir aquí
+};
+
+module.exports = nextConfig;
